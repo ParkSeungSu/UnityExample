79d9b86b787d8dea62f66baaf4ac3cff59bea446
ParkSeungSu <cldkr0401@naver.com>
2020-05-03 10:59:27 +0900

    dd

---
+++ Loglike/Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{   
+    public GameObject gameManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(GameManager.instance==null){
+            Instantiate(gameManager);
+        }
+    }
+
+   
+}
